9ccc127cddc52a7fa8474a925f51541fc75b545a
Brian Danielak <briandaniela.k+github@gmail.com>
2021-08-02 22:51:15 -0400

    Install dependencies in a separate build step

--- _build.R
+++ _build.R
@@ -1,7 +1,3 @@
-install.packages(c("bookdown", "tinytex"))
-
-tinytex::install_tinytex()
-
 bookdown::render_book('index.Rmd', 'bookdown::gitbook')
 bookdown::render_book('index.Rmd', 'bookdown::pdf_book')
 bookdown::render_book('index.Rmd', 'bookdown::epub_book')
