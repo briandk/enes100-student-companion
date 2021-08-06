476ef5d70b2864c0afb6ac795c0b5771fd6d1467
Brian Danielak <briandaniela.k+github@gmail.com>
2021-08-06 13:00:45 -0400

    Fix "Edit this page" link error
    The links used to be hard-coded to point to the `master` branch, even though the primary branch of the ENES 100 Student Companion repo is `main`
    
    See for more:
    
    https://github.com/rstudio/bookdown/issues/1220

--- _install_dependencies.R
+++ _install_dependencies.R
@@ -4,4 +4,9 @@ install.packages(c("bookdown",
                    "downlit",
                    "xml2"))
 
+# Fix issue with "Edit this page" by installing latest bookdown from CRAN
+# https://github.com/rstudio/bookdown/issues/1220
+install.packages("devtools")
+devtools::install_github("rstudio/bookdown")
+
 tinytex::install_tinytex()
