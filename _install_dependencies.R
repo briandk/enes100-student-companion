4af1c344e1e28b7f83458e5607ffdbb221d635c1
Brian Danielak <briandaniela.k+github@gmail.com>
2021-08-06 14:31:26 -0400

    Add github personal access token
    so that `devtools::install_github` has credentials to work from

--- _install_dependencies.R
+++ _install_dependencies.R
@@ -1,12 +1,8 @@
 # Necessary for `bookdown::bs4_book()` output format
 install.packages(c("bookdown",
                    "bslib",
+                   "devtools",
                    "downlit",
                    "xml2"))
-
-# Fix issue with "Edit this page" by installing latest bookdown from CRAN
-# https://github.com/rstudio/bookdown/issues/1220
-install.packages("devtools")
-devtools::install_github("rstudio/bookdown")
 
 tinytex::install_tinytex()
